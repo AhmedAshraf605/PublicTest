a7716c3f591f81d155b1f9f61a446863459300ee
Ahmed Ashraf <aashraf@instabug.com>
2024-03-26 12:56:51 +0200

    update sha256

--- InstabugCodeReplacer.rb
+++ InstabugCodeReplacer.rb
@@ -2,7 +2,7 @@ class InstabugCodeReplacer < Formula
   desc "Description of InstabugCodeReplacer Tool"
   homepage "https://www.instabug.com"
   url "https://github.com/AhmedAshraf605/PublicTest/blob/main/InstabugCodeReplacer.zip"
-#   sha256 "..." # SHA-256 hash of your binary or source code
+  sha256 "0b84412bdebbe41110c9cc31e56912911cce9515"
 
   def install
     bin.install "InstabugCodeReplacer" 
