7de84beeac23ef385f47843a5b00d2be9129b4a7
Ahmed Ashraf <aashraf@instabug.com>
2024-03-26 13:23:08 +0200

    Update

--- InstabugCodeReplacer.rb
+++ InstabugCodeReplacer.rb
@@ -1,10 +1,10 @@
-class InstabugCodeReplacer < Formula
-  desc "Description of InstabugCodeReplacer Tool"
+class TestCodeReplacer < Formula
+  desc "Description of TestCodeReplacer Tool"
   homepage "https://www.instabug.com"
   url "https://github.com/AhmedAshraf605/PublicTest/blob/main/InstabugCodeReplacer.zip"
-  sha256 "0b84412bdebbe41110c9cc31e56912911cce9515"
+  sha256 "887684c1cf94a45d99170b58468a5ae61a603d5aff94e3bdd41409b72b23be88"
 
   def install
-    bin.install "InstabugCodeReplacer" 
+    bin.install "TestCodeReplacer" 
   end
 end
