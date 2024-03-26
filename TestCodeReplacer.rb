c00448e23a22705e14f0b28379fd17a6a5adafdd
Ahmed Ashraf <aashraf@instabug.com>
2024-03-26 13:58:43 +0200

    Update

--- TestCodeReplacer.rb
+++ TestCodeReplacer.rb
@@ -1,8 +1,8 @@
 class Testcodereplacer < Formula
   desc "Description of TestCodeReplacer Tool"
   homepage "https://www.instabug.com"
-  url "https://github.com/AhmedAshraf605/PublicTest/blob/main/TestCodeReplacer.zip"
-  sha256 "9aaa3b92713565824b5e8268298b833a35a16e712b6490e964eb5b2d5573593d"
+  url "https://github.com/AhmedAshraf605/homebrew-PublicTest/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "c62d8688dee4b4b1e6735bc32045c25d2344946b9000db8890e4bacb0f8dcef2"
   version "1.0.0"
 
   def install
