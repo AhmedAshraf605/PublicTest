d00a9a44832e670b9ebc9fb3b4b1a0c47d3ad47c
Ahmed Ashraf <aashraf@instabug.com>
2024-03-26 12:09:12 +0200

    add Formula file

---
+++ InstabugCodeReplacer.rb
@@ -0,0 +1,10 @@
+class InstabugCodeReplacer < Formula
+  desc "Description of InstabugCodeReplacer Tool"
+  homepage "https://www.instabug.com"
+  url "https://github.com/AhmedAshraf605/PublicTest/blob/main/InstabugCodeReplacer.zip"
+#   sha256 "..." # SHA-256 hash of your binary or source code
+
+  def install
+    bin.install "InstabugCodeReplacer" 
+  end
+end
